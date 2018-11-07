6acd2a638e596d206c3923ae2d2f94ed973f5779
hehuan1002 <hehuan1002@126.com>
2018-11-07 19:42:45 +0800

    hash function

--- Hash/TestHash/TestHash.cpp
+++ Hash/TestHash/TestHash.cpp
@@ -2,10 +2,16 @@
 //
 
 #include "stdafx.h"
-
+#include <iostream>
+using namespace std;
+#include <functional>
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	std::string s = "Stand back! I've got jimmies!"; //key
+	std::hash<std::string> hash_fn; // hash º¯Êý
+	size_t hash = hash_fn(s); // value
+	std::cout << hash << '\n';
 	return 0;
 }
 

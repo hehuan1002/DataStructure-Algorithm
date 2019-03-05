7aae5a4f824ac52856807ab67f73eac0e5186e9b
hehuan1002 <hehuan1002@126.com>
2019-03-05 18:51:03 +0800

    Update Main.cpp

---
+++ for循环递增和递减写法的区别/Main.cpp
@@ -0,0 +1,22 @@
+// Main.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	for (int i = 20; i > 0; i--)
+	{
+		int j = 0;
+		j++;
+	}
+
+
+	for (int k = 0 ; k < 20; k++)
+	{
+		int M = 0;
+		M++;
+	}
+	return 0;
+}
+

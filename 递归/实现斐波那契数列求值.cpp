4850bd33bb8dade9e27427223655ca20fdd7131a
huan he <hehuan1002@126.com>
2019-03-30 19:43:31 +0800

    Update 实现斐波那契数列求值.cpp

--- 递归/实现斐波那契数列求值.cpp
+++ 递归/实现斐波那契数列求值.cpp
@@ -6,17 +6,17 @@
 // 
 // 当num=100时，非常非常的慢，因为有太多的重复计算。
 // TODO: 可以采用迭代或者快速幂计算，这两个方法有待完成。[3/30/2019 hehuan]
-int fab(int num)
+int Fbi(int num)
 {
 	if (num < 2)
 		return (num==0)?0:1;
 	
-	return fab(num-1)+fab(num-2);
+	return Fbi(num-1)+Fbi(num-2);
 }
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	int nResult = fab(5);
+	int nResult = Fbi(5);
 	printf("Result is :%d", nResult);
 	return 0;
 }
